40e8bca6a2a4028db8f8a61bd9f6c79d2992965c
hagler <dphagler@gmail.com>
2025-02-23 15:02:53 -0500

    new arm and elevator for testing

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,159 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmivatorConstants;
+
+public class Arm extends SubsystemBase {
+    private final SparkFlex m_armMotor;
+    private final RelativeEncoder m_relativeEncoder;
+    private final AbsoluteEncoder m_absoluteEncoder;
+
+    private static final double PIVOT_ERROR = 3;
+    private static final double GEAR_RATIO = 1;
+
+    private final SparkClosedLoopController m_sparkPidController;
+
+    private final ProfiledPIDController m_profilePID;
+
+    private final ArmFeedforward feedforward;
+    private static final double ALLOWABLE_ERROR = .5; //TODO change allowable error to make it more accurate or to make scoring faster
+    private double m_armGoalAngle = Double.MIN_VALUE;
+
+    public Arm() {
+        m_armMotor = new SparkFlex(ArmivatorConstants.armMotor, MotorType.kBrushless);
+        m_relativeEncoder = m_armMotor.getEncoder();
+        m_absoluteEncoder = m_armMotor.getAbsoluteEncoder();
+
+
+        SparkMaxConfig armConfig = new SparkMaxConfig();
+        armConfig.idleMode(IdleMode.kBrake);
+        armConfig.smartCurrentLimit(60);
+        armConfig.inverted(true);
+
+        m_sparkPidController = m_armMotor.getClosedLoopController();
+
+        armConfig.closedLoop.positionWrappingEnabled(true);
+        armConfig.closedLoop.positionWrappingMinInput(0);
+        armConfig.closedLoop.positionWrappingMaxInput(360);
+        armConfig.closedLoop.p(0.001);
+        m_profilePID = new ProfiledPIDController(0, 0, 0, new TrapezoidProfile.Constraints(.5, .5));
+        m_profilePID.enableContinuousInput(0, 360);
+        
+        // Create a new ArmFeedforward with gains kS, kG, kV, and kA
+        feedforward = new ArmFeedforward(0, 0, 0, 0);
+
+        armConfig.signals.absoluteEncoderPositionPeriodMs(20);
+        armConfig.signals.absoluteEncoderVelocityPeriodMs(20);
+
+        armConfig.encoder.positionConversionFactor(360.0 / GEAR_RATIO);
+        armConfig.encoder.velocityConversionFactor(360.0 / GEAR_RATIO / 60);
+
+        m_armMotor.configure(armConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        
+        syncRelativeEncoder();
+    }
+
+    public void moveArmToAngle(double goal) {
+
+        m_armGoalAngle = goal;
+        double currentAngle = getRelativeAngle();
+        m_profilePID.calculate(currentAngle, goal);
+        TrapezoidProfile.State setpoint = m_profilePID.getSetpoint();
+
+        //double feedForwardVolts = m_wpiFeedForward.calculateWithVelocities(currentAngle, m_relativeEncoder.getVelocity(), setpoint.velocity);
+        double feedForwardVolts = feedforward.calculate(Math.toRadians(currentAngle), Math.toRadians(setpoint.velocity));
+
+        m_sparkPidController.setReference(setpoint.position, ControlType.kPosition, ClosedLoopSlot.kSlot0, feedForwardVolts);
+        SmartDashboard.putNumber("feedForwardVolts", feedForwardVolts);
+    }
+
+
+    private void syncRelativeEncoder() {
+        m_relativeEncoder.setPosition(m_absoluteEncoder.getPosition());
+    }
+    
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Arm Angle", getRelativeAngle());
+        SmartDashboard.putNumber("Arm Velocity", getRelativeVelocity());
+        SmartDashboard.putNumber("Arm Goal Angle", m_armGoalAngle);
+    }
+
+
+    public boolean isArmAtGoal() {
+        double error = m_armGoalAngle - getRelativeAngle();
+        return Math.abs(error) < ALLOWABLE_ERROR;
+    }
+
+    public double getArmGoalAngle() {
+        return m_armGoalAngle;
+    }
+
+    public double getSpeed() {
+        return m_armMotor.getAppliedOutput();
+    }
+
+    public void stop() {
+        m_armMotor.set(0);
+    }
+
+    public void setSpeed(double speed) {
+        m_armMotor.set(speed);
+    }
+
+    public double getRelativeAngle() {
+        return m_relativeEncoder.getPosition();
+    }
+
+    public double getRelativeVelocity() {
+        return m_relativeEncoder.getVelocity();
+    }
+
+    public double getAbsoluteAngle() {
+        return m_absoluteEncoder.getPosition();
+    }
+
+    private void resetPidController() {
+        m_profilePID.reset(getRelativeAngle(), getRelativeVelocity());
+    }
+
+    public boolean isAtGoalAngle() {
+        return (Math.abs(this.getRelativeAngle() - this.getArmGoalAngle()) <= Arm.PIVOT_ERROR);
+    }
+
+    ////////////////
+    //command factories yay :))
+    ////////////////
+    ///
+    public Command createMoveArmtoAngleCommand(Double angle) {
+        return createResetPidControllerCommand().andThen(runEnd(() -> moveArmToAngle(angle), this::stop)).withName("Go to angle" + angle);
+    }
+
+    private Command createResetPidControllerCommand() {
+        return runOnce(this::resetPidController);
+    }
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,168 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmivatorConstants;
+
+import com.playingwithfusion.TimeOfFlight;
+
+import com.revrobotics.RelativeEncoder;
+
+public class Elevator extends SubsystemBase {
+
+    public static final double K_ELEVATOR_GEARING = 4.0;
+    public static final double K_CARRIAGE_MASS = 2.0; // kg
+    public static final double K_MIN_ELEVATOR_HEIGHT = Units.inchesToMeters(0);
+    public static final double K_MAX_ELEVATOR_HEIGHT = Units.inchesToMeters(20  );
+    public static final DCMotor K_ELEVATOR_GEARBOX = DCMotor.getVex775Pro(4);
+    public static final double K_ELEVATOR_DRUM_RADIUS = Units.inchesToMeters(2.0);
+    public static final double ELEVATOR_ERROR = Units.inchesToMeters(3);
+
+
+    private final SparkFlex m_elevatorMotor;    
+    private final RelativeEncoder m_encoder;
+    private TimeOfFlight m_elevatorSensor;
+
+    private final ProfiledPIDController m_profilePID;
+    private final SparkClosedLoopController m_sparkPidController;
+    private final ElevatorFeedforward feedforward;
+
+    private double m_goalHeight;
+
+
+    public Elevator() {
+
+        m_elevatorMotor = new SparkFlex(ArmivatorConstants.elevatorMotor, MotorType.kBrushless);
+        m_encoder = m_elevatorMotor.getEncoder();
+        m_elevatorSensor = new TimeOfFlight(4);
+
+        SparkMaxConfig elevatorConfig = new SparkMaxConfig();
+        
+        elevatorConfig
+          .idleMode(IdleMode.kBrake)
+          .smartCurrentLimit(60)
+          .inverted(true)
+          .softLimit
+          .reverseSoftLimit(0)
+          .reverseSoftLimitEnabled(true);
+
+        elevatorConfig
+          .absoluteEncoder
+          .positionConversionFactor(1.375);//4 to 1 gear ratio .8755 radius spool
+        
+        elevatorConfig
+          .closedLoop
+          .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+          .p(.001);
+
+        m_sparkPidController = m_elevatorMotor.getClosedLoopController();
+        elevatorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder);
+
+        m_profilePID = new ProfiledPIDController(0, 0, 0, new TrapezoidProfile.Constraints(0.5, 0.5));
+
+        // Create a new ElevatorFeedforward with gains kS, kG, kV, and kA
+        feedforward = new ElevatorFeedforward(0, 0, 0, 0);
+
+        m_elevatorMotor.configure(elevatorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    }
+
+    public double getHeight() {
+        return m_encoder.getPosition();
+    }
+
+    public void setSpeed(double speed) {
+        m_elevatorMotor.set(speed);
+    }
+
+    public double getElevatorDistanceInInch(){
+
+      return (m_elevatorSensor.getRange()*0.03937008)-2;
+          
+    }
+
+    public boolean isAtBottom() {
+        return getElevatorDistanceInInch()<.1;
+    }
+
+    public boolean isAtTop() {
+        return getElevatorDistanceInInch()>=20;
+    }
+
+    public void stop() {
+        m_elevatorMotor.set(0);
+    }
+
+    public double getGoalHeight() {
+        return m_goalHeight;
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Elevator Height", getHeight());
+        SmartDashboard.putNumber("Elevator Distance", getElevatorDistanceInInch());
+        SmartDashboard.putBoolean("Elevator at bottom", isAtBottom());
+        SmartDashboard.putBoolean("Elevator at top", isAtTop());
+        SmartDashboard.putNumber("Elevator Goal Height", m_goalHeight);
+    }
+
+    public double getEncoderVel() {
+        return m_encoder.getVelocity();
+    }
+
+    public void goToHeight(double goalHeight) {
+        m_profilePID.calculate(getHeight(), goalHeight);
+        m_goalHeight = goalHeight;
+        TrapezoidProfile.State setpoint = m_profilePID.getSetpoint();
+
+        double feedForwardVolts = feedforward.calculateWithVelocities(
+            getEncoderVel(),
+            setpoint.velocity);
+
+        m_sparkPidController.setReference(setpoint.position, ControlType.kPosition, ClosedLoopSlot.kSlot0, feedForwardVolts);
+        SmartDashboard.putNumber("feedForwardVolts", feedForwardVolts);
+
+    }
+
+    public void resetPidController() {
+        m_profilePID.reset(getHeight(), getEncoderVel());
+    }
+
+    public boolean isAtGoalHeight() {
+        return Math.abs(getHeight() - m_goalHeight) <= ELEVATOR_ERROR;
+    }
+
+    //command factories//
+    public Command createResetPidControllerCommand() {
+        return runOnce(this::resetPidController);
+    }
+
+    public Command createMoveElevatorToHeightCommand(double height) {
+        return createResetPidControllerCommand().andThen(
+        runEnd(() -> goToHeight(height), m_elevatorMotor::stopMotor)).withName("Elevator go to height" + height);
+    }
+
+
+}
