8b36db35e3b5c6eec9826692ad399e4c452ff991
hagler <dphagler@gmail.com>
2025-03-18 20:12:45 -0400

    1234

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -41,14 +41,7 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
 
-    /*
-     * This example of adding Limelight is very simple and may not be sufficient for on-field use.
-     * Users typically need to provide a standard deviation that scales with the distance to target
-     * and changes with number of tags available.
-     *
-     * This example is sufficient to show that vision integration is possible, though exact implementation
-     * of how to use vision should be tuned per-robot and to the team's specification.
-     */
+    
     if (kUseLimelight) 
     {
       var driveState = RobotContainer.drivetrain.getState();
